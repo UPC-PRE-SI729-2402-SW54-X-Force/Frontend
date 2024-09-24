2d2eb320fbb1c2999154af713240ec4c01edc488
Al3xiel <gabriel280104casas@gmail.com>
2024-09-24 18:30:11 -0500

    feat(user-management): Added user entity.

---
+++ src/app/user-management/model/user.entity.ts
@@ -0,0 +1,14 @@
+export class User{
+  id: number;
+  name: string;
+  age: number;
+  email: string;
+  address: string;
+  constructor() {
+    this.id = 0;
+    this.name = "";
+    this.age = 0;
+    this.address = "";
+    this.email = "";
+  }
+}
