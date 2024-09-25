92526977e08a9731253250ddf297851506903dc8
Jamir <u201824409@upc.edu.pe>
2024-09-25 16:01:54 -0500

    feat(resource.entity.ts): Added a new attribute.

--- src/app/resources/model/resource.entity.ts
+++ src/app/resources/model/resource.entity.ts
@@ -2,10 +2,12 @@ export class Resource {
   image: string;
   title: string;
   description: string;
+  url: string
 
-  constructor(resource:{image: string, title: string, description: string}) {
+  constructor(resource:{image: string, title: string, description: string, url: string}) {
     this.image = resource.image;
     this.title = resource.title;
     this.description = resource.description;
+    this.url = resource.url;
   }
 }
