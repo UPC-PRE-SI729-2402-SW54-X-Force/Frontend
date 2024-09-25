886883cef7bf28360e3733c846aa5a9aaade908b
Al3xiel <gabriel280104casas@gmail.com>
2024-09-24 23:40:49 -0500

    feat(user-basic-information-display): implemented the user basic information display..

--- src/app/user-management/pages/user/user.component.ts
+++ src/app/user-management/pages/user/user.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UsersService} from "../../services/users.service";
+import {User} from "../../model/user.entity";
+import {Subscription} from "rxjs";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatList, MatListItem, MatListItemLine, MatListItemTitle} from "@angular/material/list";
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [
+    MatDividerModule,
+    MatList,
+    MatListItem,
+    MatListItemTitle,
+    MatListItemLine,
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+
+  user: User;
+  subscription!: Subscription;
+  constructor(private userService: UsersService) {
+    this.user = new User();
+  }
+  ngOnInit(): void {
+    this.subscription = this.userService.getById(1).subscribe((data: User) => {
+      this.user = data;
+    });
+  }
 
 }
