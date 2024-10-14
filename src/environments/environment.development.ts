8318ccfb40bba30f27dbca1d70176373223b9fcd
Al3xiel <gabriel280104casas@gmail.com>
2024-10-13 22:52:25 -0500

    feat(user-management): added the email address and profile picture edit capability.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serverBasePath: 'https://my-json-server.typicode.com/UPC-PRE-SI729-2402-SW54-X-Force/server'
+  serverBasePath: ' http://localhost:3000/api/v1'
 }
