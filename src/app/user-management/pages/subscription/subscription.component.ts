32dbff63f96c2d11d0cb6c81b25e45e25516d59e
Al3xiel <gabriel280104casas@gmail.com>
2024-10-14 19:55:40 -0500

    feat(user-management): added the user subscriptions.

--- src/app/user-management/pages/subscription/subscription.component.ts
+++ src/app/user-management/pages/subscription/subscription.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatCard} from "@angular/material/card";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-subscription',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatButton
+  ],
   templateUrl: './subscription.component.html',
   styleUrl: './subscription.component.css'
 })
