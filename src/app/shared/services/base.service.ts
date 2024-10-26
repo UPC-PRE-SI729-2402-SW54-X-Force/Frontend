22d73e528cf0026c309e3cff903fb6c8ad6b5dc9
Jamir <u201824409@upc.edu.pe>
2024-10-26 05:47:08 -0500

    feat(user-management): Added login functionality

--- src/app/shared/services/base.service.ts
+++ src/app/shared/services/base.service.ts
@@ -4,65 +4,65 @@ import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
 import {catchError, Observable, retry, throwError} from "rxjs";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class BaseService<T> {
 
-    protected basePath: string = `${environment.serverBasePath}`;
+  protected basePath: string = `${environment.serverBasePath}`;
 
-    protected resourceEndpoint: string = '/resources';
+  protected resourceEndpoint: string = '/resources';
 
-    protected httpOptions = {
-        headers: new HttpHeaders({
-            'Content-type': 'application/json',
-        })
+  protected httpOptions = {
+    headers: new HttpHeaders({
+      'Content-type': 'application/json',
+    })
+  }
+
+  protected http: HttpClient = inject(HttpClient);
+
+  protected resourcePath(): string {
+    return `${this.basePath}${this.resourceEndpoint}`;
+  }
+
+
+  protected handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Client-side error:', error.error.message);
+    } else {
+      console.error(`Server-side error: Code ${error.status}, Message: ${error.message}, Body: ${error.error}`);
     }
-
-    protected http: HttpClient = inject(HttpClient);
-
-    protected resourcePath(): string {
-        return `${this.basePath}${this.resourceEndpoint}`;
-    }
+    return throwError(() => new Error('Something happened with request, please try again later'));
+  }
 
 
-    protected handleError(error: HttpErrorResponse) {
-        if (error.error instanceof ErrorEvent) {
-            console.error('Client-side error:', error.error.message);
-        } else {
-            console.error(`Server-side error: Code ${error.status}, Message: ${error.message}, Body: ${error.error}`);
-        }
-        return throwError(() => new Error('Something happened with request, please try again later'));
-    }
+  // Create Resource
+  public create(item: any): Observable<T> {
+    return this.http.post<T>(this.resourcePath(), JSON.stringify(item), this.httpOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
 
+  // Delete Resource
+  public delete(id: any): Observable<any> {
+    return this.http.delete(`${this.resourcePath()}/${id}`, this.httpOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
 
-    // Create Resource
-    public create(item: any): Observable<T> {
-        return this.http.post<T>(this.resourcePath(), JSON.stringify(item), this.httpOptions)
-            .pipe(retry(2), catchError(this.handleError));
-    }
+  // Update Resource
+  public update(id: any, item: any): Observable<T> {
+    return this.http.put<T>(`${this.resourcePath()}/${id}`, JSON.stringify(item), this.httpOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
 
-    // Delete Resource
-    public delete(id: any) {
-        return this.http.delete(`${this.resourcePath()}/${id}`, this.httpOptions)
-            .pipe(retry(2), catchError(this.handleError));
-    }
+  // Get All Resources
+  public getAll(): Observable<Array<T>> {
+    return this.http.get<Array<T>>(this.resourcePath(), this.httpOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
 
-    // Update Resource
-    public update(id: any, item: any): Observable<T> {
-        return this.http.put<T>(`${this.resourcePath()}/${id}`, JSON.stringify(item), this.httpOptions)
-            .pipe(retry(2), catchError(this.handleError));
-    }
-
-    // Get All Resources
-    public getAll(): Observable<T> {
-        return this.http.get<T>(this.resourcePath(), this.httpOptions)
-            .pipe(retry(2), catchError(this.handleError));
-    }
-
-    // Get Resource By Id
-    public getById(id: any): Observable<T> {
-        return this.http.get<T>(`${this.resourcePath()}/${id}`)
-            .pipe(retry(2), catchError(this.handleError));
-    }
+  // Get Resource By Id
+  public getById(id: any): Observable<T> {
+    return this.http.get<T>(`${this.resourcePath()}/${id}`)
+      .pipe(retry(2), catchError(this.handleError));
+  }
 
 }
--- src/app/user-management/components/login-form/login-form.component.ts
+++ src/app/user-management/components/login-form/login-form.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatButton} from "@angular/material/button";
 import {MatInput} from "@angular/material/input";
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import {UsersService} from "../../services/users.service";
+import {FormsModule} from "@angular/forms";
+import {User} from "../../model/user.entity";
 
 @Component({
   selector: 'app-login-form',
@@ -12,14 +15,43 @@ import {RouterLink} from "@angular/router";
     MatButton,
     MatInput,
     MatLabel,
-    RouterLink
+    RouterLink,
+    FormsModule
   ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
 export class LoginFormComponent {
+  email: string = '';
+  password: string = '';
 
-  validateUser() {
+  constructor(private userService: UsersService, private router: Router) {}
 
+  login() {
+    // Intentar con el endpoint de "arrendatarios"
+    this.userService.setResourceEndPoint('/users2');
+    this.userService.getAll().subscribe((users: any[]) => {
+      let user = users.find((u: any) => u.email === this.email && u.password === this.password);
+
+      if (user) {
+        console.log('Login exitoso como arrendatario');
+        this.router.navigate(['/home']);
+      } else {
+        // Intentar con el endpoint de "dueños de vehículos"
+        this.userService.setResourceEndPoint('/users1');
+        this.userService.getAll().subscribe((users: any[]) => {
+          user = users.find((u: any) => u.email === this.email && u.password === this.password);
+
+          if (user) {
+            console.log('Login exitoso como dueño de vehículo');
+            this.router.navigate(['/home']);
+          } else {
+            console.error('Credenciales incorrectas');
+            alert('Email o contraseña incorrectos.');
+          }
+        });
+      }
+    });
   }
+
 }
