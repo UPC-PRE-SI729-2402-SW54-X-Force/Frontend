c9e6e2fe158aef525c745e04809ccf87b11de0cd
GeorgeGC23 <gc_oso123@hotmail.com>
2024-10-26 21:04:33 -0500

    feat: update db and offers filters

--- src/app/public/pages/offers/offer.model.ts
+++ src/app/public/pages/offers/offer.model.ts
@@ -1,13 +1,17 @@
 export interface Offer {
   id: number;
-  owner: string;
-  car: {
-    model: string;
-    year: number;
-    image: string;
-  };
+  user_id: number;
+  owner?: string;
+  car: string;
+  model: string;
+  year: number;
+  image: string;
   title: string;
   subtitle: string;
   description: string;
   status: string;
+  destination: string;
+  date: Date;
+  insurance: string;
+  premium: string;
 }
--- src/app/public/pages/offers/offers.component.ts
+++ src/app/public/pages/offers/offers.component.ts
@@ -32,8 +32,10 @@ import { Offer } from './offer.model';
 })
 export class OffersComponent implements OnInit, AfterViewInit {
   offers: Offer[] = [];
+  filteredOffers: Offer[] = [];
   destination: string = '';
   date: Date | null = null;
+  modelFilter: string = ''
 
   @ViewChild('picker', { static: true }) datepicker!: MatDatepicker<any>;
 
@@ -49,13 +51,33 @@ export class OffersComponent implements OnInit, AfterViewInit {
     }, 0);
   }
 
-  onDestinationChange(value: string) {
-    this.destination = value;
-  }
-
   getOffers(): void {
     this.http.get<Offer[]>('http://localhost:3000/offers').subscribe((data: Offer[]) => {
       this.offers = data;
+      this.applyFilters();
     });
+  }
+
+  onDestinationChange(value: string) {
+    this.destination = value;
+    this.applyFilters(); // Aplicar filtros al cambiar destino
+  }
+
+  onModelFilterChange(value: string) {
+    this.modelFilter = value;
+    this.applyFilters(); // Aplicar filtros al cambiar modelo
+  }
+
+  onDateChange(value: Date | null) {
+    this.date = value;
+    this.applyFilters(); // Aplicar filtros al cambiar fecha
+  }
+
+  applyFilters(): void {
+    this.filteredOffers = this.offers.filter(offer =>
+      (this.destination ? offer.destination.toLowerCase().includes(this.destination.toLowerCase()) : true) &&
+      (this.modelFilter ? offer.model.toLowerCase().includes(this.modelFilter.toLowerCase()) : true) &&
+      (this.date ? new Date(offer.date).toDateString() === this.date.toDateString() : true)
+    );
   }
 }
