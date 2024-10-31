fa5699c5643aefbc36b7c6079a03e2c489d03810
Jamir <u201824409@upc.edu.pe>
2024-10-31 14:27:07 -0500

    feat(car-management): Added tenants page.

--- src/app/car-management/pages/tenant-list/tenant-list.component.ts
+++ src/app/car-management/pages/tenant-list/tenant-list.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from "../../../user-management/model/user.entity";
+import {UserService} from "../../../user-management/services/user.service";
+import {CarItemComponent} from "../../components/car-item/car-item.component";
+import {MatButton} from "@angular/material/button";
+import {NgForOf} from "@angular/common";
+import {TenantItemComponent} from "../../components/tenant-item/tenant-item.component";
 
 @Component({
   selector: 'app-tenant-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CarItemComponent,
+    MatButton,
+    NgForOf,
+    TenantItemComponent
+  ],
   templateUrl: './tenant-list.component.html',
   styleUrl: './tenant-list.component.css'
 })
-export class TenantListComponent {
+export class TenantListComponent implements OnInit {
 
+  tenants!: User[];
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.loadTenants();
+  }
+
+  private loadTenants(): void {
+    this.userService.getAll().subscribe((data: User[]) => (this.tenants = data));
+  }
 }
