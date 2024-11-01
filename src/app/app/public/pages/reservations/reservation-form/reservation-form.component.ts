753e9e1ac13cec4fd1d758f095ebbbd14d679506
GeorgeGC23 <118072852+AldoGC23@users.noreply.github.com>
2024-11-01 14:50:04 -0500

    feat: added reservation module

---
+++ src/app/app/public/pages/reservations/reservation-form/reservation-form.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation-form',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      reservation-form works!
+    </p>
+  `,
+  styles: ``
+})
+export class ReservationFormComponent {
+
+}
